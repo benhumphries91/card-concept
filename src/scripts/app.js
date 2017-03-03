7b6d46c0e9ec03abfe91f6fc1cc2ecfd47b9f242
Ben Humphries <ben.humphries@stoneshot.com>
2017-03-03 16:04:19 +0000

    Card Concept inital commit

---
+++ src/scripts/app.js
@@ -0,0 +1,27 @@
+(function(){
+	var app = angular.module('lujo', []);
+
+	app.controller('PropertyController', function(){
+		this.villa = property;
+	});
+
+	var property = [
+		{
+			name: 'Hotel 1',
+			price: 295,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in nisl ligula. Sed tempor gravida iaculis. Integer at tortor posuere, posuere lorem id, pellentesque sapien. Sed efficitur dui at magna tempor luctus. Suspendisse et placerat libero. Sed ultrices arcu mauris, quis sodales nisl egestas nec. Fusce mattis, lorem ac laoreet egestas, ligula libero accumsan arcu, eu bibendum orci dui nec risus. Sed sit amet efficitur lorem. Phasellus consequat sed tortor a vehicula. Aliquam a diam condimentum, sodales sem eget, commodo dui.',
+			image: 'hotel-1',
+		}, {
+			name: 'Hotel 2',
+			price: 350,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in nisl ligula. Sed tempor gravida iaculis. Integer at tortor posuere, posuere lorem id, pellentesque sapien. Sed efficitur dui at magna tempor luctus. Suspendisse et placerat libero. Sed ultrices arcu mauris, quis sodales nisl egestas nec. Fusce mattis, lorem ac laoreet egestas, ligula libero accumsan arcu, eu bibendum orci dui nec risus. Sed sit amet efficitur lorem. Phasellus consequat sed tortor a vehicula. Aliquam a diam condimentum, sodales sem eget, commodo dui.',
+			image: 'hotel-2',
+		}, {
+			name: 'Hotel 3',
+			price: 500,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in nisl ligula. Sed tempor gravida iaculis. Integer at tortor posuere, posuere lorem id, pellentesque sapien. Sed efficitur dui at magna tempor luctus. Suspendisse et placerat libero. Sed ultrices arcu mauris, quis sodales nisl egestas nec. Fusce mattis, lorem ac laoreet egestas, ligula libero accumsan arcu, eu bibendum orci dui nec risus. Sed sit amet efficitur lorem. Phasellus consequat sed tortor a vehicula. Aliquam a diam condimentum, sodales sem eget, commodo dui.',
+			image: 'hotel-3',
+		}
+	];
+
+})();
